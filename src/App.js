a4e005e79a279a6d9fd2b3f248fd02e6085eb125
jgcapdev <jorge.gomez-carrillo-external@capgemini.com>
2022-08-03 11:30:59 +0200

    feat: adding basic scene

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Canvas, extend } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [8, 1.5, 8], fov: 25 }}>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+
+      <OrbitControls makeDefault />
+    </Canvas>
   );
 }
 
