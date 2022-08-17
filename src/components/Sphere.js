e802b6637013f8920af6da5fec32a252bfa74048
Jorge <jorge.gomez-carrillo@capgemini.com>
2022-08-17 11:31:24 +0200

    fix: sphere

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -1,12 +1,10 @@
 import { useSphere } from '@react-three/cannon';
 import { useFrame } from '@react-three/fiber';
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { usePlayerControls } from '../utils/useControls';
 import * as THREE from 'three';
 
 const Sphere = (props) => {
-  const [event, setEvent] = useState('');
-
   const direction = new THREE.Vector3();
   const frontVector = new THREE.Vector3();
   const sideVector = new THREE.Vector3();
@@ -16,16 +14,14 @@ const Sphere = (props) => {
   const [ref, api] = useSphere((index) => ({
     mass: 1,
     onCollide: (e) => {
+      console.log(e);
       // Be carefull, the ids will change if you add a new geometry
-      if (e.contact.bj.id === 56) {
+      if (e.contact.bj.id === 66) {
         console.log('Contacto con amarillo');
-        setEvent('Contacto con amarillo');
-      } else if (e.contact.bj.id === 57) {
+      } else if (e.contact.bj.id === 67) {
         console.log('Contacto con rojo');
-        setEvent('Contacto con rojo');
-      } else if (e.contact.bj.id === 58) {
+      } else if (e.contact.bj.id === 68) {
         console.log('Contacto con verde');
-        setEvent('Contacto con verde');
       }
     },
     position: props.position,
